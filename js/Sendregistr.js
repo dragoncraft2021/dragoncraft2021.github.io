ca8c3f7566fecb106d7642531f1c4ce7aa937e83
DragonCraft-Daniil <84133012+dragoncraft2021@users.noreply.github.com>
2021-07-06 11:57:55 +0300

    Add files via upload

--- js/Sendregistr.js
+++ js/Sendregistr.js
@@ -42,7 +42,7 @@ $('#sendMail').on("click", function() {
             }else{
                 $('#mailForm').trigger('reset');
 
-                window.location.href = "http://github.com/dragoncraft2021/dragoncraft2021.github.io/code.php";
+                window.location.href = "http://dragoncraft2021.github.io/code.php";
             }
             $("#sendMail").prop("disabled", false);
         }
