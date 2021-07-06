6c27fd1f0e2e699f59cea6e26091aef6b370916c
DragonCraft-Daniil <84133012+dragoncraft2021@users.noreply.github.com>
2021-07-06 12:01:33 +0300

    Add files via upload

--- register.php
+++ register.php
@@ -62,6 +62,6 @@
         </div>
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="js/script1.js"></script>
+    <script src="js/Sendregistr.js"></script>
 </body>
 </html>
